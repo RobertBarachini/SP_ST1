bc962323ef6fc07f802c9816e57ca8191a89633b
Aleksandar Hristov <ah6874@student.uni-lj.si>
2018-12-09 18:30:22 +0000

    l

--- app_server/routes/posts.route.js
+++ app_server/routes/posts.route.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var postsController = require('../controllers/posts.controller')
 
 /* GET home page. */
-router.get('/postID', postsController.postPage);
+router.get('/:postID', postsController.postPage);
 router.get('/postID/editPost', postsController.editPost);
 router.post('/postID/editPost', postsController.editPostPost);
 router.post('/postID', postsController.addComment)
