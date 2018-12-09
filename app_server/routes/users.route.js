4e1c8c488faad5f149fca7a74bea7a99aaf0d204
AndrijaV <andrija.vu4ko@gmail.com>
2018-12-09 22:14:27 +0000

    popravki

--- app_server/routes/users.route.js
+++ app_server/routes/users.route.js
@@ -4,8 +4,8 @@ var usersController = require('../controllers/users.controller')
 
 /* GET home page. */
 router.get('/:userID', usersController.userPage);
-router.get('/userID/editProfile', usersController.editProfile);
-router.post('/userID/editProfile', usersController.editProfilePost);
+router.get('/:userID/editProfile', usersController.editProfile);
+router.post('/:userID/editProfile', usersController.editProfilePost);
 router.get('/logout', usersController.logout);
 router.get('/userID/addPicture',usersController.addPicture);
 router.get('/userID/addEmbed',usersController.addEmbed);
