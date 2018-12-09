e9ab0ebf5e2a43c695d58cf226ae65572a1475a1
AndrijaV <andrija.vu4ko@gmail.com>
2018-12-09 16:17:37 +0000

    za push in preveri

--- app_server/routes/users.route.js
+++ app_server/routes/users.route.js
@@ -5,6 +5,7 @@ var usersController = require('../controllers/users.controller')
 /* GET home page. */
 router.get('/userID', usersController.userPage);
 router.get('/userID/editProfile', usersController.editProfile);
+router.post('/userID/editProfile', usersController.editProfilePost);
 router.get('/logout', usersController.logout);
 
 
