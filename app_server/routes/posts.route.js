ae987b76251456e1a4725073fe83ad55207e446f
robi <rm0595@student.uni-lj.si>
2018-12-10 01:11:09 +0000

    delni delete post

--- app_server/routes/posts.route.js
+++ app_server/routes/posts.route.js
@@ -4,9 +4,11 @@ var postsController = require('../controllers/posts.controller')
 
 /* GET home page. */
 router.get('/:postID', postsController.postPage);
+router.delete('/:postID', postsController.deletePost)
 router.get('/postID/editPost', postsController.editPost);
 router.post('/postID/editPost', postsController.editPostPost);
 router.post('/postID', postsController.addComment)
+
 
 
 module.exports = router;
