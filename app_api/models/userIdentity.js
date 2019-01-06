4c8f51857043b28a22955650df8f11755045ba7d
Robert Barachini <robert.barachini@gmail.com>
2019-01-06 12:29:18 +0000

    Update userIdentity model

--- app_api/models/userIdentity.js
+++ app_api/models/userIdentity.js
@@ -4,7 +4,9 @@ var mongoose = require('mongoose');
 var userIdentityShema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
   password: {type: String, required: true},
-  userType: {type: String, required: false}
+  salt: {type: String, required: true},
+  userType: {type: String, required: false, default: "user"}
 });
+// userType: user / admin - if user is not logged in we do not assume his identity
 
 mongoose.model('UserIdentity', userIdentityShema, 'UserIdentities');
