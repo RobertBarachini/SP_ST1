20ca5c5fb3edb544b6cb6c01d99112f642f974ad
AndrijaV <andrija.vu4ko@gmail.com>
2019-01-12 21:59:51 +0000

    db response

--- app_client/dbt/dbt.controller.js
+++ app_client/dbt/dbt.controller.js
@@ -1,14 +1,39 @@
 (function(){
     function dbtCtrl($http){
         var vm = this;
+        vm.responseD='';
+        vm.responseI='';
         vm.init = function(){
+            
+        vm.responseD='';
+        vm.responseI='';
             console.log("init");
-            return $http.get("/api/db/init")
+            //return $http.get("/api/db/init")
+            $http.get("/api/db/init").then(
+                function success(res){
+                    vm.responseI='success';
+                },
+                  function error(er){
+                    vm.responseI='error';
+                    console.error(er);
+                  } 
+            );
         }
         
         vm.drop = function(){
+            
+        vm.responseD='';
+        vm.responseI='';
             console.log("drop");
-            return $http.get("/api/db/drop")
+            $http.get("/api/db/drop").then(
+                function success(res){
+                    vm.responseD='success';
+                },
+                  function error(er){
+                    vm.responseD='error';
+                    console.error(er);
+                  } 
+            );
         }
     }
     
