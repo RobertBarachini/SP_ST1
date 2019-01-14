2e1f6dbb3fb940851cbd7b405e3d96187c0c3f23
Robert Barachini <robert.barachini@gmail.com>
2019-01-14 10:35:14 +0000

    Update

--- app_client/dbt/dbt.controller.js
+++ app_client/dbt/dbt.controller.js
@@ -1,4 +1,12 @@
 (function(){
+    function sleep(milliseconds) {
+      var start = new Date().getTime();
+      for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+          break;
+        }
+      }
+    }
     function dbtCtrl($http){
         var vm = this;
         vm.responseD='';
@@ -11,11 +19,13 @@
             //return $http.get("/api/db/init")
             $http.get("/api/db/init").then(
                 function success(res){
+                    sleep(7000);
                     vm.responseI='success';
                 },
                   function error(er){
+                    sleep(7000);
                     vm.responseI='error';
-                    console.error(er);
+                    //console.error(er);
                   } 
             );
         }
@@ -27,11 +37,13 @@
             console.log("drop");
             $http.get("/api/db/drop").then(
                 function success(res){
+                    sleep(7000);
                     vm.responseD='success';
                 },
                   function error(er){
+                    sleep(7000);
                     vm.responseD='error';
-                    console.error(er);
+                    //console.error(er);
                   } 
             );
         }
